fb9aeda41fbec79c86b39576fd092b7245b7e438
Neville Nazerane <neville@mediroutes.com>
2024-08-03 15:37:46 -0700

    creating empty args

---
+++ src/AzBlobTool/Models/EmptyArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzBlobTool.Models
+{
+    public class EmptyArgs : ArgsBase
+    {
+
+
+
+    }
+}
--- src/AzBlobTool/Program.cs
+++ src/AzBlobTool/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+
+
+using AzBlobTool.Models;
+using System.Net.WebSockets;
+
 Console.WriteLine("Hello, World!");
+
+ArgsBase BuildArgs()
+{
+    if (args.Length < 2)
+    {
+        Console.WriteLine("Invalid command");
+        PrintInstructions();
+        return null;
+    }
+
+    var command = args.First();
+    if (!Enum.TryParse<ValidArgTypes>(command, out var cmd))
+    {
+        Console.WriteLine($"'{command}' is an invalid command");
+        PrintValidCommands();
+        return null;
+    }
+
+
+    var result = cmd switch
+    {
+        ValidArgTypes.Empty => CreateEmptyArgs(),
+        //ValidArgTypes.Upload => throw new NotImplementedException(),
+        _ => null
+    };
+
+
+    return null;
+}
+
+
+static EmptyArgs CreateEmptyArgs()
+{
+    return new();
+} 
+
+static void PrintInstructions()
+{
+    Console.WriteLine("Valid syntax:");
+    Console.WriteLine("azblobez <command> <connection string> [arguments]");
+    PrintValidCommands();
+}
+
+static void PrintValidCommands()
+{
+    Console.WriteLine("Valid commands:");
+    foreach (var valid in Enum.GetNames<ValidArgTypes>())
+        Console.WriteLine($" {valid}");
+}
